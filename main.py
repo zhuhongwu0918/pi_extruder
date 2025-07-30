8df2931f9cf99f11d4510959005d6fdefcb44fdd
Hans <zhu-hongwu@qq.com>
2025-07-30 20:56:49 +0800

    ui整理到根目录

---
+++ main.py
@@ -0,0 +1,27 @@
+import logging
+import sys
+import os
+# import app_ui
+from main_application import MainApplication
+import signal
+# 配置日志
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(threadName)s] %(message)s",
+    handlers=[logging.StreamHandler(sys.stdout)]
+)
+def signal_handler(sig, frame):
+    logging.warning("捕获到Ctrl+C信号")
+    app.safe_shutdown()
+
+if __name__ == "__main__":
+    app = MainApplication()
+    # 设置Ctrl+C信号处理
+    try:
+        signal.signal(signal.SIGINT, signal_handler)
+    except ImportError:
+        pass  # Windows系统可能需要额外的处理
+    # 运行主循环
+    app.mainloop()
+    # 确保完全退出
+    os._exit(0)
